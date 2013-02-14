f02ac5620e4455214d0ac5e6ce6bcdc416605d88
Jinwook Shin <jinwook@skoopmedia.co.kr>
2013-02-15 01:17:53 +0900

    first commit

---
+++ BouncyCastle/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 제어됩니다. 
+// 어셈블리와 연결된 정보를 수정하려면
+// 이러한 특성 값을 변경하십시오.
+[assembly: AssemblyTitle("BouncyCastle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BouncyCastle")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible을 false로 설정하면 이 어셈블리의 형식이 
+// COM 구성 요소에 표시되지 않습니다. COM에서 이 어셈블리의 형식에 액세스해야 할 경우 
+// 해당 형식에 대해 ComVisible 특성을 true로 설정하십시오.
+[assembly: ComVisible(false)]
+
+// 이 프로젝트가 COM에 노출된 경우 다음 GUID는 typelib의 ID에 사용됩니다.
+[assembly: Guid("b15206ec-8cce-47ee-baba-876e09331b2a")]
+
+// 어셈블리에 대한 버전 정보는 다음 네 개의 값으로 구성됩니다.
+//
+//      주 버전
+//      부 버전 
+//      빌드 번호
+//      수정 버전
+//
+// 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 수정 버전 및 빌드 번호가 자동으로 
+// 지정되도록 할 수 있습니다.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("ko-KR")]
